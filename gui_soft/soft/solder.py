7d452378e35510137247f1a9ab2e9857becd142b
Aram Poghosyan <aram.poghosyan@realschool.am>
2020-11-23 23:23:57 +0400

    changed Gui.

--- gui_soft/soft/solder.py
+++ gui_soft/soft/solder.py
@@ -13,18 +13,21 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(706, 342)
+        Dialog.resize(706, 338)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("../Pictures/Webcam/2020-07-01-203153.jpg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap("image/icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         Dialog.setWindowIcon(icon)
-        Dialog.setStyleSheet("background-color: rgb(156, 155, 96);")
+        Dialog.setStyleSheet("background-image: url(\"/workspace/solder/gui_soft/soft/image/background.png\"); background-repeat: no-repeat; \n"
+"background-position: center")
         self.tabWidget = QtWidgets.QTabWidget(Dialog)
         self.tabWidget.setGeometry(QtCore.QRect(10, 10, 681, 321))
+        self.tabWidget.setStyleSheet("gridline-color: rgba(13, 13, 15, 128);")
         self.tabWidget.setObjectName("tabWidget")
         self.tab_4 = QtWidgets.QWidget()
         self.tab_4.setObjectName("tab_4")
         self.hot_gun_on_off = QtWidgets.QPushButton(self.tab_4)
         self.hot_gun_on_off.setGeometry(QtCore.QRect(540, 10, 121, 41))
+        self.hot_gun_on_off.setStyleSheet("  border-radius: 10px;")
         self.hot_gun_on_off.setObjectName("hot_gun_on_off")
         self.groupBox = QtWidgets.QGroupBox(self.tab_4)
         self.groupBox.setGeometry(QtCore.QRect(10, 50, 221, 201))
@@ -94,9 +97,11 @@ class Ui_Dialog(object):
         self.label.setObjectName("label")
         self.comboBox = QtWidgets.QComboBox(self.tab_4)
         self.comboBox.setGeometry(QtCore.QRect(10, 20, 101, 25))
+        self.comboBox.setStyleSheet("border-radius: 10px;")
         self.comboBox.setObjectName("comboBox")
         self.groupBox_2 = QtWidgets.QGroupBox(self.tab_4)
         self.groupBox_2.setGeometry(QtCore.QRect(260, 50, 231, 201))
+        self.groupBox_2.setStyleSheet("")
         self.groupBox_2.setObjectName("groupBox_2")
         self.fan_speed_slider = QtWidgets.QSlider(self.groupBox_2)
         self.fan_speed_slider.setGeometry(QtCore.QRect(10, 140, 211, 16))
@@ -129,9 +134,6 @@ class Ui_Dialog(object):
 "}")
         self.fan_speed_slider.setOrientation(QtCore.Qt.Horizontal)
         self.fan_speed_slider.setObjectName("fan_speed_slider")
-        self.dial = QtWidgets.QDial(self.groupBox_2)
-        self.dial.setGeometry(QtCore.QRect(130, 70, 50, 64))
-        self.dial.setObjectName("dial")
         self.splitter_3 = QtWidgets.QSplitter(self.groupBox_2)
         self.splitter_3.setGeometry(QtCore.QRect(10, 30, 151, 41))
         self.splitter_3.setOrientation(QtCore.Qt.Horizontal)
@@ -150,23 +152,24 @@ class Ui_Dialog(object):
         self.label_3.setObjectName("label_3")
         self.pushButton = QtWidgets.QPushButton(self.tab_4)
         self.pushButton.setGeometry(QtCore.QRect(120, 20, 71, 25))
+        self.pushButton.setStyleSheet("  border-radius: 10px;")
         self.pushButton.setObjectName("pushButton")
         self.tabWidget.addTab(self.tab_4, "")
         self.tab_3 = QtWidgets.QWidget()
         self.tab_3.setObjectName("tab_3")
         self.widget = QtWidgets.QWidget(self.tab_3)
-        self.widget.setGeometry(QtCore.QRect(10, 20, 351, 261))
+        self.widget.setGeometry(QtCore.QRect(0, 10, 361, 271))
         self.widget.setObjectName("widget")
         self.groupBox_3 = QtWidgets.QGroupBox(self.tab_3)
-        self.groupBox_3.setGeometry(QtCore.QRect(370, 0, 301, 151))
+        self.groupBox_3.setGeometry(QtCore.QRect(370, 10, 301, 171))
         self.groupBox_3.setObjectName("groupBox_3")
-        self.widget1 = QtWidgets.QWidget(self.groupBox_3)
-        self.widget1.setGeometry(QtCore.QRect(0, 20, 291, 131))
-        self.widget1.setObjectName("widget1")
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.widget1)
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.pid_p_splitter = QtWidgets.QSplitter(self.widget1)
+        self.layoutWidget = QtWidgets.QWidget(self.groupBox_3)
+        self.layoutWidget.setGeometry(QtCore.QRect(10, 20, 281, 141))
+        self.layoutWidget.setObjectName("layoutWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.layoutWidget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        self.pid_p_splitter = QtWidgets.QSplitter(self.layoutWidget)
         self.pid_p_splitter.setOrientation(QtCore.Qt.Horizontal)
         self.pid_p_splitter.setObjectName("pid_p_splitter")
         self.pid_p_slider = QtWidgets.QSlider(self.pid_p_splitter)
@@ -174,41 +177,66 @@ class Ui_Dialog(object):
         self.pid_p_slider.setOrientation(QtCore.Qt.Horizontal)
         self.pid_p_slider.setObjectName("pid_p_slider")
         self.pid_p_lcd = QtWidgets.QLCDNumber(self.pid_p_splitter)
+        self.pid_p_lcd.setStyleSheet("QLCDNumber{\n"
+"  background-color: rgb(217, 214, 28);\n"
+"  border: 2px solid rgb(113, 113, 113);\n"
+"  border-width: 2px;\n"
+"  border-radius: 10px;\n"
+"  color: rgb(255, 255, 255);\n"
+"}\n"
+"")
         self.pid_p_lcd.setObjectName("pid_p_lcd")
         self.label_4 = QtWidgets.QLabel(self.pid_p_splitter)
         self.label_4.setObjectName("label_4")
-        self.verticalLayout.addWidget(self.pid_p_splitter)
-        self.pid_i_splitter = QtWidgets.QSplitter(self.widget1)
+        self.gridLayout.addWidget(self.pid_p_splitter, 1, 0, 1, 1)
+        self.pid_i_splitter = QtWidgets.QSplitter(self.layoutWidget)
         self.pid_i_splitter.setOrientation(QtCore.Qt.Horizontal)
         self.pid_i_splitter.setObjectName("pid_i_splitter")
         self.pid_i_slider = QtWidgets.QSlider(self.pid_i_splitter)
+        self.pid_i_slider.setMouseTracking(True)
         self.pid_i_slider.setOrientation(QtCore.Qt.Horizontal)
         self.pid_i_slider.setObjectName("pid_i_slider")
         self.pid_i_lcd = QtWidgets.QLCDNumber(self.pid_i_splitter)
+        self.pid_i_lcd.setStyleSheet("QLCDNumber{\n"
+"  background-color: rgb(217, 214, 28);\n"
+"  border: 2px solid rgb(113, 113, 113);\n"
+"  border-width: 2px;\n"
+"  border-radius: 10px;\n"
+"  color: rgb(255, 255, 255);\n"
+"}\n"
+"")
         self.pid_i_lcd.setObjectName("pid_i_lcd")
         self.label_5 = QtWidgets.QLabel(self.pid_i_splitter)
         self.label_5.setObjectName("label_5")
-        self.verticalLayout.addWidget(self.pid_i_splitter)
-        self.pid_d_splitter = QtWidgets.QSplitter(self.widget1)
+        self.gridLayout.addWidget(self.pid_i_splitter, 2, 0, 1, 1)
+        self.pid_d_splitter = QtWidgets.QSplitter(self.layoutWidget)
         self.pid_d_splitter.setOrientation(QtCore.Qt.Horizontal)
         self.pid_d_splitter.setObjectName("pid_d_splitter")
         self.pid_d_slider = QtWidgets.QSlider(self.pid_d_splitter)
         self.pid_d_slider.setOrientation(QtCore.Qt.Horizontal)
         self.pid_d_slider.setObjectName("pid_d_slider")
         self.pid_d_lcd = QtWidgets.QLCDNumber(self.pid_d_splitter)
+        self.pid_d_lcd.setStyleSheet("QLCDNumber{\n"
+"  background-color: rgb(217, 214, 28);\n"
+"  border: 2px solid rgb(113, 113, 113);\n"
+"  border-width: 2px;\n"
+"  border-radius: 10px;\n"
+"  color: rgb(255, 255, 255);\n"
+"}\n"
+"")
         self.pid_d_lcd.setObjectName("pid_d_lcd")
         self.label_6 = QtWidgets.QLabel(self.pid_d_splitter)
         self.label_6.setObjectName("label_6")
-        self.verticalLayout.addWidget(self.pid_d_splitter)
+        self.gridLayout.addWidget(self.pid_d_splitter, 0, 0, 1, 1)
         self.tabWidget.addTab(self.tab_3, "")
 
         self.retranslateUi(Dialog)
-        self.tabWidget.setCurrentIndex(1)
+        self.tabWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        Dialog.setWindowTitle(_translate("Dialog", "Soldering Station"))
         self.hot_gun_on_off.setText(_translate("Dialog", "On"))
         self.groupBox.setTitle(_translate("Dialog", "Hot Air"))
         self.label_2.setText(_translate("Dialog", "Set Temp"))
@@ -217,7 +245,7 @@ class Ui_Dialog(object):
         self.label_3.setText(_translate("Dialog", "Fan Speed"))
         self.pushButton.setText(_translate("Dialog", "Connect"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), _translate("Dialog", "Main"))
-        self.groupBox_3.setTitle(_translate("Dialog", "GroupBox"))
+        self.groupBox_3.setTitle(_translate("Dialog", "PID Correction"))
         self.label_4.setText(_translate("Dialog", "Proportional"))
         self.label_5.setText(_translate("Dialog", "Integral"))
         self.label_6.setText(_translate("Dialog", "derivative"))

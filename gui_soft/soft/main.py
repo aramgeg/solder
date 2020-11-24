71070dd294505ae95a873d38cfe5a93871b86d73
Aram Poghosyan <aram.poghosyan@realschool.am>
2020-11-24 23:45:46 +0400

    Added Pid control part.

--- gui_soft/soft/main.py
+++ gui_soft/soft/main.py
@@ -38,6 +38,67 @@ class Worker(QObject):
         self.on_off_state = 1
         self.set_temp_plot=[]
         self.real_temp_plot=[]
+        #Connect PID sliders response
+        prog.pid_p_slider.valueChanged.connect(self.pid_p_slider)
+        prog.pid_i_slider.valueChanged.connect(self.pid_i_slider)
+        prog.pid_d_slider.valueChanged.connect(self.pid_d_slider)
+        prog.pid_write_settings_btn.clicked.connect(self.write_pid_settings)
+        #Pid_values
+        self.pid_p_value=0
+        self.pid_i_value=0
+        self.pid_d_value=0
+        self.get_init_values()
+
+    def get_init_values(self):
+        print("Get values inital values ")
+        try:
+            self.init_data = self.ser.readline().decode('utf-8')
+            pass
+        except Exception as e:
+            exit()
+            pass
+        else:
+            pass
+        finally:
+            pass
+        self.init_values = self.init_data.split(",")
+        #Set temperature display
+        prog.set_temp_slider.setValue(float(self.init_values[0]))
+        #Set air speed value
+        prog.fan_speed_slider.setValue(float(self.init_values[2]))
+        #Set PID values
+        prog.pid_p_slider.setValue(float(self.init_values[4])*10)
+        prog.pid_i_slider.setValue(float(self.init_values[5])*10)
+        prog.pid_d_slider.setValue(float(self.init_values[6])*10)
+
+    def pid_p_slider(self):
+        self.pid_p_value = prog.pid_p_slider.value()/10
+        prog.pid_p_lcd.display(self.pid_p_value)
+        #print(self.pid_p_value,",",self.pid_i_value,",",self.pid_d_value)
+        self.write_pid_value()
+
+    def pid_i_slider(self):
+        self.pid_i_value = prog.pid_i_slider.value()/10
+        prog.pid_i_lcd.display(self.pid_i_value)
+        #print(self.pid_p_value,",",self.pid_i_value,",",self.pid_d_value)
+        self.write_pid_value()
+
+    def pid_d_slider(self):
+        self.pid_d_value = prog.pid_d_slider.value()/10
+        prog.pid_d_lcd.display(self.pid_d_value)
+        #print(self.pid_p_value,",",self.pid_i_value,",",self.pid_d_value)
+        self.write_pid_value()
+    
+    def write_pid_settings(self):
+        print("Save button")
+        self.write_pid_value("write")
+
+    def write_pid_value(self,write=""):
+        #print("Writing pid value")
+        self.ser.write(str.encode("pid,"+str(self.pid_p_value)+','+str(self.pid_i_value)+','+ str(self.pid_d_value)\
+            +','+str(write)+'\r\n'))
+
+
     def work(self):
         while self.working:
             self.read_data()
@@ -49,6 +110,7 @@ class Worker(QObject):
     def read_data(self):
         #Reading Serial Data from Arduino
         self.data = self.ser.readline().decode('utf-8')
+        #print(self.data)
         #Getting sliders current postition value
         self.set_temp_slider=str(prog.set_temp_slider.value())
         self.set_air_speed_slider=str(prog.fan_speed_slider.value())
@@ -71,10 +133,10 @@ class Worker(QObject):
         self.set_temp_uart=str(self.set_temp_slider_value)
         self.set_air_speed_uart=str(self.set_air_speed_slider_value)
         self.set_on_off_uart=str(self.on_off_state)
-        print(self.set_on_off_uart)
+        #print(self.set_on_off_uart)
         self.ser.write(str.encode(self.set_temp_uart+','+self.set_air_speed_uart+','+ self.set_on_off_uart+'\r\n'))
         #self.ser.write(str.encode(self.set_temp_uart+','+self.set_air_speed_uart+','+ '0' +'\r\n'))
-        print("Updating values")
+        #print("Updating values")
             #self.ser.write(str.encode('200,100,1\r\n'))
             #print(self.set_temp_slider_value)
         #time.sleep(0.2)
@@ -101,12 +163,14 @@ class Worker(QObject):
         self.values = self.data.split(",")
 
         #print(int(self.values[3]))
-        """
+        """        
         if(self.prev_set_temp != float(self.values[0])):
+            #prog.set_temp.display(int(self.values[0]))
+            prog.set_temp_slider.setValue(float(self.values[0]))
+            print("set temp slider changed")
+            print(self.values[0])
+            time.sleep(2)
             self.prev_set_temp=float(self.values[0])
-            prog.set_temp.display(float(self.values[0]))
-            #prog.set_temp_slider.setValue(float(self.values[0]))
-            print("Update set temp")
         """
         if(int(self.values[3]) == 1):
             prog.hot_gun_on_off.setText("On")
--- gui_soft/soft/solder.py
+++ gui_soft/soft/solder.py
@@ -13,21 +13,23 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(706, 338)
+        Dialog.resize(706, 340)
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap("image/icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         Dialog.setWindowIcon(icon)
-        Dialog.setStyleSheet("background-image: url(\"/workspace/solder/gui_soft/soft/image/background.png\"); background-repeat: no-repeat; \n"
+        Dialog.setStyleSheet("background-image: url(\"/workspace/solder/gui_soft/soft/image/background.png\"); \n"
+"background-repeat: no-repeat; \n"
 "background-position: center")
         self.tabWidget = QtWidgets.QTabWidget(Dialog)
         self.tabWidget.setGeometry(QtCore.QRect(10, 10, 681, 321))
-        self.tabWidget.setStyleSheet("gridline-color: rgba(13, 13, 15, 128);")
+        self.tabWidget.setStyleSheet("selection-color: rgb(164, 0, 0);")
         self.tabWidget.setObjectName("tabWidget")
         self.tab_4 = QtWidgets.QWidget()
         self.tab_4.setObjectName("tab_4")
         self.hot_gun_on_off = QtWidgets.QPushButton(self.tab_4)
         self.hot_gun_on_off.setGeometry(QtCore.QRect(540, 10, 121, 41))
-        self.hot_gun_on_off.setStyleSheet("  border-radius: 10px;")
+        self.hot_gun_on_off.setStyleSheet("border-radius: 10px;\n"
+"background-color: rgba(0, 0, 0,140);")
         self.hot_gun_on_off.setObjectName("hot_gun_on_off")
         self.groupBox = QtWidgets.QGroupBox(self.tab_4)
         self.groupBox.setGeometry(QtCore.QRect(10, 50, 221, 201))
@@ -61,7 +63,12 @@ class Ui_Dialog(object):
 "border-top-left-radius: 9px;\n"
 "border-bottom-left-radius: 9px;\n"
 "}")
+        self.set_temp_slider.setMinimum(140)
+        self.set_temp_slider.setMaximum(480)
+        self.set_temp_slider.setSingleStep(10)
         self.set_temp_slider.setOrientation(QtCore.Qt.Horizontal)
+        self.set_temp_slider.setTickPosition(QtWidgets.QSlider.TicksBothSides)
+        self.set_temp_slider.setTickInterval(10)
         self.set_temp_slider.setObjectName("set_temp_slider")
         self.splitter_2 = QtWidgets.QSplitter(self.groupBox)
         self.splitter_2.setGeometry(QtCore.QRect(10, 30, 161, 41))
@@ -96,7 +103,7 @@ class Ui_Dialog(object):
         self.label = QtWidgets.QLabel(self.splitter)
         self.label.setObjectName("label")
         self.comboBox = QtWidgets.QComboBox(self.tab_4)
-        self.comboBox.setGeometry(QtCore.QRect(10, 20, 101, 25))
+        self.comboBox.setGeometry(QtCore.QRect(10, 20, 141, 25))
         self.comboBox.setStyleSheet("border-radius: 10px;")
         self.comboBox.setObjectName("comboBox")
         self.groupBox_2 = QtWidgets.QGroupBox(self.tab_4)
@@ -132,6 +139,9 @@ class Ui_Dialog(object):
 "border-top-left-radius: 9px;\n"
 "border-bottom-left-radius: 9px;\n"
 "}")
+        self.fan_speed_slider.setMinimum(20)
+        self.fan_speed_slider.setMaximum(100)
+        self.fan_speed_slider.setPageStep(5)
         self.fan_speed_slider.setOrientation(QtCore.Qt.Horizontal)
         self.fan_speed_slider.setObjectName("fan_speed_slider")
         self.splitter_3 = QtWidgets.QSplitter(self.groupBox_2)
@@ -139,19 +149,21 @@ class Ui_Dialog(object):
         self.splitter_3.setOrientation(QtCore.Qt.Horizontal)
         self.splitter_3.setObjectName("splitter_3")
         self.fan_speed = QtWidgets.QLCDNumber(self.splitter_3)
-        self.fan_speed.setStyleSheet("QLCDNumber{\n"
+        self.fan_speed.setStyleSheet("/*\n"
+"QLCDNumber{\n"
 "  background-color: rgb(217, 214, 28);\n"
 "  border: 2px solid rgb(113, 113, 113);\n"
 "  border-width: 2px;\n"
 "  border-radius: 10px;\n"
 "  color: rgb(255, 255, 255);\n"
-"}\n"
+"}*/\n"
 "")
         self.fan_speed.setObjectName("fan_speed")
         self.label_3 = QtWidgets.QLabel(self.splitter_3)
+        self.label_3.setStyleSheet("selection-background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(0, 0, 0, 255), stop:1 rgba(255, 255, 255, 255));")
         self.label_3.setObjectName("label_3")
         self.pushButton = QtWidgets.QPushButton(self.tab_4)
-        self.pushButton.setGeometry(QtCore.QRect(120, 20, 71, 25))
+        self.pushButton.setGeometry(QtCore.QRect(160, 20, 71, 25))
         self.pushButton.setStyleSheet("  border-radius: 10px;")
         self.pushButton.setObjectName("pushButton")
         self.tabWidget.addTab(self.tab_4, "")
@@ -161,19 +173,25 @@ class Ui_Dialog(object):
         self.widget.setGeometry(QtCore.QRect(0, 10, 361, 271))
         self.widget.setObjectName("widget")
         self.groupBox_3 = QtWidgets.QGroupBox(self.tab_3)
-        self.groupBox_3.setGeometry(QtCore.QRect(370, 10, 301, 171))
+        self.groupBox_3.setGeometry(QtCore.QRect(370, 10, 301, 261))
         self.groupBox_3.setObjectName("groupBox_3")
         self.layoutWidget = QtWidgets.QWidget(self.groupBox_3)
-        self.layoutWidget.setGeometry(QtCore.QRect(10, 20, 281, 141))
+        self.layoutWidget.setGeometry(QtCore.QRect(10, 20, 291, 181))
         self.layoutWidget.setObjectName("layoutWidget")
         self.gridLayout = QtWidgets.QGridLayout(self.layoutWidget)
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
         self.gridLayout.setObjectName("gridLayout")
-        self.pid_p_splitter = QtWidgets.QSplitter(self.layoutWidget)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.splitter_4 = QtWidgets.QSplitter(self.layoutWidget)
+        self.splitter_4.setOrientation(QtCore.Qt.Vertical)
+        self.splitter_4.setObjectName("splitter_4")
+        self.pid_p_splitter = QtWidgets.QSplitter(self.splitter_4)
         self.pid_p_splitter.setOrientation(QtCore.Qt.Horizontal)
         self.pid_p_splitter.setObjectName("pid_p_splitter")
         self.pid_p_slider = QtWidgets.QSlider(self.pid_p_splitter)
-        self.pid_p_slider.setMaximum(10)
+        self.pid_p_slider.setMaximum(100)
+        self.pid_p_slider.setSingleStep(1)
         self.pid_p_slider.setOrientation(QtCore.Qt.Horizontal)
         self.pid_p_slider.setObjectName("pid_p_slider")
         self.pid_p_lcd = QtWidgets.QLCDNumber(self.pid_p_splitter)
@@ -188,12 +206,12 @@ class Ui_Dialog(object):
         self.pid_p_lcd.setObjectName("pid_p_lcd")
         self.label_4 = QtWidgets.QLabel(self.pid_p_splitter)
         self.label_4.setObjectName("label_4")
-        self.gridLayout.addWidget(self.pid_p_splitter, 1, 0, 1, 1)
-        self.pid_i_splitter = QtWidgets.QSplitter(self.layoutWidget)
+        self.pid_i_splitter = QtWidgets.QSplitter(self.splitter_4)
         self.pid_i_splitter.setOrientation(QtCore.Qt.Horizontal)
         self.pid_i_splitter.setObjectName("pid_i_splitter")
         self.pid_i_slider = QtWidgets.QSlider(self.pid_i_splitter)
         self.pid_i_slider.setMouseTracking(True)
+        self.pid_i_slider.setMaximum(100)
         self.pid_i_slider.setOrientation(QtCore.Qt.Horizontal)
         self.pid_i_slider.setObjectName("pid_i_slider")
         self.pid_i_lcd = QtWidgets.QLCDNumber(self.pid_i_splitter)
@@ -208,11 +226,11 @@ class Ui_Dialog(object):
         self.pid_i_lcd.setObjectName("pid_i_lcd")
         self.label_5 = QtWidgets.QLabel(self.pid_i_splitter)
         self.label_5.setObjectName("label_5")
-        self.gridLayout.addWidget(self.pid_i_splitter, 2, 0, 1, 1)
-        self.pid_d_splitter = QtWidgets.QSplitter(self.layoutWidget)
+        self.pid_d_splitter = QtWidgets.QSplitter(self.splitter_4)
         self.pid_d_splitter.setOrientation(QtCore.Qt.Horizontal)
         self.pid_d_splitter.setObjectName("pid_d_splitter")
         self.pid_d_slider = QtWidgets.QSlider(self.pid_d_splitter)
+        self.pid_d_slider.setMaximum(100)
         self.pid_d_slider.setOrientation(QtCore.Qt.Horizontal)
         self.pid_d_slider.setObjectName("pid_d_slider")
         self.pid_d_lcd = QtWidgets.QLCDNumber(self.pid_d_splitter)
@@ -227,7 +245,11 @@ class Ui_Dialog(object):
         self.pid_d_lcd.setObjectName("pid_d_lcd")
         self.label_6 = QtWidgets.QLabel(self.pid_d_splitter)
         self.label_6.setObjectName("label_6")
-        self.gridLayout.addWidget(self.pid_d_splitter, 0, 0, 1, 1)
+        self.verticalLayout.addWidget(self.splitter_4)
+        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
+        self.pid_write_settings_btn = QtWidgets.QPushButton(self.groupBox_3)
+        self.pid_write_settings_btn.setGeometry(QtCore.QRect(10, 210, 111, 31))
+        self.pid_write_settings_btn.setObjectName("pid_write_settings_btn")
         self.tabWidget.addTab(self.tab_3, "")
 
         self.retranslateUi(Dialog)
@@ -249,6 +271,7 @@ class Ui_Dialog(object):
         self.label_4.setText(_translate("Dialog", "Proportional"))
         self.label_5.setText(_translate("Dialog", "Integral"))
         self.label_6.setText(_translate("Dialog", "derivative"))
+        self.pid_write_settings_btn.setText(_translate("Dialog", "Write Settings"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), _translate("Dialog", "Plot"))
 
 
